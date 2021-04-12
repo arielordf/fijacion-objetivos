515524ec3bdf12f268da165576f8db6646e061f0
RUIZ DIAZ FABIO Jose Ariel <ariel.ruiz@tigo.net.py>
2021-04-12 10:37:42 -0400

    fix

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function generarObjetivo() {
 
   var uno = ["Investigar", "Aprender", "Mejorar"];
   var dos = [" mi ingles", " el cluster de desarrollo", " Curso en Millicom University"];
-  var dos = [" mi ingles", " el cluster de desarrollo", " Curso en Millicom University"];
   var tres = [" a la mañana", " a la siesta", " despues de la ofi"];
 
   var unoLength = parseInt(uno.length);
   var dosLength = parseInt(dos.length);
   var tresLength = parseInt(tres.length);
+  
   var rndUno = Math.floor(Math.random()* unoLength );
   var rndDos = Math.floor(Math.random()* dosLength );
   var rndTres = Math.floor(Math.random()* tresLength );
