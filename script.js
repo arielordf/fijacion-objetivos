218baaee4866a6cf37c8eb96a10efec75228b776
matirivas1995 <matias.rt@hotmail.com>
2021-04-12 14:32:03 -0400

    cambios visuales

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function generarObjetivo() {
 
-  var uno = ["Investigar", "Aprender", "Mejorar"];
-  var dos = [" mi ingles", " el cluster de desarrollo", " Curso en Millicom University", " Angular", " Kubernetes", " Tests Unitarios"];
-  var tres = [" a la mañana", " a la siesta", " despues de la ofi", " entre pacientes"];
+  var uno = ["Investigar", "Aprender", "Mejorar", "Presentar", "Crear", "Depurar", "Arreglar"];
+  var dos = [" mi ingles", " el cluster de desarrollo"," Mi tesis", " Guarani"," ishus del sonar", " Proyectos paralelos", " Curso en Millicom University", " Angular", " Kubernetes", " Tests Unitarios"];
+  var tres = [" a la mañana","en el almuerzo","durante mis vacaciones", " en mi flex", " a la siesta", " despues de la ofi", " entre pacientes", " de madrugada", " en el after"];
 
   var unoLength = parseInt(uno.length);
   var dosLength = parseInt(dos.length);
@@ -11,6 +11,5 @@ function generarObjetivo() {
   var rndUno = Math.floor(Math.random()* unoLength );
   var rndDos = Math.floor(Math.random()* dosLength );
   var rndTres = Math.floor(Math.random()* tresLength );
-  
   document.getElementById('display').innerText = uno[rndUno] + dos[rndDos] + tres[rndTres];
 }
